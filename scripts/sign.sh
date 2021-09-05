7aa79f3a5ea08eb43debf89ee8a226a6c5b9969b
Cyclic3 <cyclic3.git@gmail.com>
2021-09-05 22:48:20 +0100

    Added signing

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -1,2 +1,6 @@
-gpg --default-key "University of Liverpool Cybersecurity Society Apt Repo" -abs -o - Release > Release.gpg
-gpg --default-key "University of Liverpool Cybersecurity Society Apt Repo" --clearsign -o - Release > InRelease
+echo "Copy Release to the signer, and run the following commands:"
+cat << 'EOF'
+  gpg --default-key "University of Liverpool Cybersecurity Society Apt Repo" -abs -o - Release > Release.gpg
+  gpg --default-key "University of Liverpool Cybersecurity Society Apt Repo" --clearsign -o - Release > InRelease
+EOF
+echo Copy the resultant files back here
